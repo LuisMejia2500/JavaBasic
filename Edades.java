dcfb4d49f258fa727e6b060dd87bc473f54be6b5
Luis Gustavo Mejía Barrios <71474291+LuisMejia2500@users.noreply.github.com>
2020-09-26 17:22:17 -0500

    Add files via upload

---
+++ Edades.java
@@ -0,0 +1,14 @@
+class Edades {
+  public static void main(String[] args) {
+    int edad;
+    edad=18;
+    if(edad>=18){
+      System.out.println("Usted es mayor de edad");
+    }else{
+      System.out.println("Usted es menor de edad");
+    }
+
+    
+    }
+
+  }
